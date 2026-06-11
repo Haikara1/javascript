20ebaab1409396c134a51c806251ba7e9efda53c
Daniel Lima <danisfilholima@gmail.com>
2026-06-11 19:24:34 -0300

    repetições com while, do..while e for em JS

---
+++ CURSOJS/aula013/ambiente.js
@@ -0,0 +1,24 @@
+// ESTRUTURA COM TESTE DEPOIS
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+// Estrutura com TESTE PRIMEIRO
+// var c = 1
+// while (c <= 500) {
+//     console.log(`Passo ${c}`)
+//     c++
+// };
+
+// ESTRUTURA NAO USADA
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
