0edfd85152c414e3fcf9b8bea6f5a4148bf1a0ab
Daniel Lima <danisfilholima@gmail.com>
2026-06-30 15:07:51 -0300

    async e await javascript

---
+++ CURSOJS/async/script.js
@@ -0,0 +1,44 @@
+// sintaxe
+// function primeiraFuncao() {
+
+//     return new Promise((resolve, reject) => {
+
+//         setTimeout(() => {
+//             console.log("Esperou isso aqui")
+//             resolve()
+//         }, 1000)
+//     })
+// }
+
+// async function segundaFuncao() {
+
+//     console.log("Iniciou")
+
+//    await primeiraFuncao()
+
+//     console.log("Terminou")
+
+// }
+
+// segundaFuncao()
+
+// prático
+function getUser(id) {
+
+    return fetch(`https://dummyjson.com/users/${id}`)
+        .then(data => data.json())
+        .catch(err => console.log(err))
+}
+
+async function showUserName(id) {
+
+    try {
+        const user = await getUser(id)
+
+        console.log(`O nome do usuário é: ${user.first_name}`)
+    } catch(err) {
+        console.log(`Erro: ${err}`)
+    }
+}
+
+showUserName(3)
